fe322abdcf960ad72685deb73e07fddb157d321c
User <User@User>
2018-09-12 12:43:19 +0700

    TampilHasil

--- src/dynamic_programming/Tugas01.java
+++ src/dynamic_programming/Tugas01.java
@@ -1,11 +1,13 @@
 
 package dynamic_programming;
+import java.math.BigInteger;
 import java.util.Scanner;
 public class Tugas01 {
     private static void main(String[] args) {
         String identitas = "Krisvany Afril Patricia/X RPL 3/17";
         tampilJudul(identitas);
         int n = tampilInput();
+        BigInteger hasil = fibo(n);
     }
     public static void tampilJudul(String identitas) {
         System.out.println("Identitas :" + identitas);
@@ -24,5 +26,17 @@ public class Tugas01 {
         return n;
         
     }
+  private  static BigInteger fibo(int n) {
+      
+      BigInteger[] hasil  = new BigInteger[n];
+      
+      hasil[0] = BigInteger.ONE;
+      hasil[1] = BigInteger.ONE;
+      
+      for (int i = 2; i< n;i++) {
+          hasil[i] = hasil[i-1].add(hasil[i-2]);
+      }
+      return hasil[n-1];
+  }
     
 }
