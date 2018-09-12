43f6b195b7632de44060df9caaf0d624fc59f299
User <User@User>
2018-09-12 12:34:15 +0700

    Tambah Input

--- src/dynamic_programming/Tugas01.java
+++ src/dynamic_programming/Tugas01.java
@@ -1,11 +1,11 @@
 
 package dynamic_programming;
-
+import java.util.Scanner;
 public class Tugas01 {
     private static void main(String[] args) {
         String identitas = "Krisvany Afril Patricia/X RPL 3/17";
         tampilJudul(identitas);
-        
+        int n = tampilInput();
     }
     public static void tampilJudul(String identitas) {
         System.out.println("Identitas :" + identitas);
@@ -13,6 +13,15 @@ public class Tugas01 {
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
         
+        
+    }
+    private static int tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-:");
+        int n = scanner.nextInt();
+        
+        return n;
         
     }
     
