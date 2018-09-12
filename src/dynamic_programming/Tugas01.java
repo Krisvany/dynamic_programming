c5b45235886e7a2251399a5e969d0bd203c82930
User <User@User>
2018-09-12 12:51:42 +0700

    Tampil Hasil

--- src/dynamic_programming/Tugas01.java
+++ src/dynamic_programming/Tugas01.java
@@ -8,6 +8,7 @@ public class Tugas01 {
         tampilJudul(identitas);
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n,hasil);
     }
     public static void tampilJudul(String identitas) {
         System.out.println("Identitas :" + identitas);
@@ -38,6 +39,8 @@ public class Tugas01 {
       }
       return hasil[n-1];
   }
-  
+  private static void tampilHasil(int n, BigInteger hasil) {
+      System.out.println("Bilangan Fibonacci ke-" +n+" : "+ hasil);
+  }
     
 }
