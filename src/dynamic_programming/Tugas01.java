173e3bca537407e4aee34a10656d89812589706c
User <User@User>
2018-09-12 11:26:08 +0700

    Jobsheet7

---
+++ src/dynamic_programming/Tugas01.java
@@ -0,0 +1,19 @@
+
+package dynamic_programming;
+
+public class Tugas01 {
+    private static void main(String[] args) {
+        String identitas = "Krisvany Afril Patricia/X RPL 3/17";
+        tampilJudul(identitas);
+        
+    }
+    public static void tampilJudul(String identitas) {
+        System.out.println("Identitas :" + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+        
+        
+    }
+    
+}
